9e20b34fd5094e07d37a06aea017b347bb6f6d8c
rustem sirazetdinov <avesirazetdinov@gmail.com>
2025-02-26 13:29:13 +0300

    wip: teacher fixes 1

--- practice-3-Multi-Inheritance/main.cpp
+++ practice-3-Multi-Inheritance/main.cpp
@@ -23,9 +23,51 @@ struct A1 {
         int a1;
 };
 
-struct B1 : public A1 {
+struct A2 {
     public:
-        B1 (int m) : A1(m), b1(m)
+        A2 (int m) : a2(m)
+        {
+            std::cout << std::format("A2 ctor: A2({})\n", m);
+        }
+
+        virtual void printn (std::ostream& os = std::cout)
+        {
+            os << std::format("'printn' from A2, A2::a2 = {};\n", a2);
+        }
+
+        virtual void show (std::ostream& os = std::cout)
+        {
+            os << std::format("Available variables from A2: A2::a2 = {};\n", a2);
+        }
+
+    protected:
+        int a2;
+};
+
+struct A3 {
+    public:
+        A3 (int m) : a3(m)
+        {
+            std::cout << std::format("A3 ctor: A3({})\n", m);
+        }
+
+        virtual void printn (std::ostream& os = std::cout)
+        {
+            os << std::format("'printn' from A3, A3::a3 = {};\n", a3);
+        }
+
+        virtual void show (std::ostream& os = std::cout)
+        {
+            os << std::format("Available variables from A3: A3::a3 = {};\n", a3);
+        }
+
+    protected:
+        int a3;
+};
+
+struct B1 : public A1, virtual public A3 {
+    public:
+        B1 (int m) : A3(m), A1(m), b1(m)
         {
             std::cout << std::format("B1 ctor: B1({})\n", m);
         }
@@ -37,16 +79,37 @@ struct B1 : public A1 {
 
         virtual void show (std::ostream& os = std::cout)
         {
-            os << std::format("Available variables from B1: A1::a1 = {}, B1::b1 = {};\n", A1::a1, b1);
+            os << std::format("Available variables from B1: A1::a1 = {}, A3::a3 = {}, B1::b1 = {};\n", A1::a1, A3::a3, b1);
         }
 
     protected:
         int b1;
 };
 
-struct C1 : public B1 {
+struct B2 : public A2, virtual public A3 {
     public:
-        C1 (int m) : B1(m), c1(m)
+        B2 (int m) : A3(m), A2(m), b2(m)
+        {
+            std::cout << std::format("B2 ctor: B2({})\n", m);
+        }
+
+        virtual void printn (std::ostream& os = std::cout)
+        {
+            os << std::format("'printn' from B2, B2::b2 = {};\n", b2);
+        }
+
+        virtual void show (std::ostream& os = std::cout)
+        {
+            os << std::format("Available variables from B2: A2::a2 = {}, A3::a3 = {}, B2::b2 = {};\n", A2::a2, A3::a3, b2);
+        }
+
+    protected:
+        int b2;
+};
+
+struct C1 : public B1, public B2 {
+    public:
+        C1 (int m) : A3(-1), B1(m), B2(m), c1(m)
         {
             std::cout << std::format("С1 ctor: С1({})\n", m);
         }
@@ -58,18 +121,24 @@ struct C1 : public B1 {
 
         virtual void show (std::ostream& os = std::cout)
         {
-            os << std::format("Available variables from C1: A1::a1 = {}, B1::b1 = {}, C1::c1 = {};\n", A1::a1, B1::b1, c1);
+            os << "Available variables from C1:\n"
+                << std::format("\tB1::A1::a1 = {},\n", B1::A1::a1)
+                << std::format("\tB2::A2::a2 = {},\n", B2::A2::a2)
+                << std::format("\tB1::A3::a3 = B2::A3::a3 {}, {},\n", B1::A3::a3, B2::A3::a3)
+                << std::format("\tB1::b1 = {},\n", B1::b1)
+                << std::format("\tB2::b2 = {},\n", B2::b2)
+                << std::format("\tC1::c1 = {};\n", c1);
         }
 
     protected:
         int c1;
 };
 
-struct C2 : public B1 {
+struct C2 : public B1, public B2 {
     public:
-        C2 (int m) : B1(m), c2(m)
+        C2 (int m) : A3(-2), B1(m), B2(m), c2(m)
         {
-            std::cout << std::format("C2 ctor: C2({})\n", m);
+            std::cout << std::format("С2 ctor: С2({})\n", m);
         }
 
         virtual void printn (std::ostream& os = std::cout)
@@ -79,11 +148,44 @@ struct C2 : public B1 {
 
         virtual void show (std::ostream& os = std::cout)
         {
-            os << std::format("Available variables from C2: A1::a1 = {}, B1::b1 = {}, C2::c2 = {};\n", A1::a1, B1::b1, c2);
+            os << "Available variables from C2:\n"
+                << std::format("\tB1::A1::a1 = {},\n", B1::A1::a1)
+                << std::format("\tB2::A2::a2 = {},\n", B2::A2::a2)
+                << std::format("\tB1::A3::a3 = B2::A3::a3 {}, {},\n", B1::A3::a3, B2::A3::a3)
+                << std::format("\tB1::b1 = {},\n", B1::b1)
+                << std::format("\tB2::b2 = {},\n", B2::b2)
+                << std::format("\tC2::c2 = {};\n", c2);
         }
 
     protected:
         int c2;
+};
+
+struct C3 : public B1, public B2 {
+    public:
+        C3 (int m) : A3(-3), B1(m), B2(m), c3(m)
+        {
+            std::cout << std::format("С3 ctor: С3({})\n", m);
+        }
+
+        virtual void printn (std::ostream& os = std::cout)
+        {
+            os << std::format("'printn' from C3, C3::c3 = {};\n", c3);
+        }
+
+        virtual void show (std::ostream& os = std::cout)
+        {
+            os << "Available variables from C3:\n"
+                << std::format("\tB1::A1::a1 = {},\n", B1::A1::a1)
+                << std::format("\tB2::A2::a2 = {},\n", B2::A2::a2)
+                << std::format("\tB1::A3::a3 = B2::A3::a3 {}, {},\n", B1::A3::a3, B2::A3::a3)
+                << std::format("\tB1::b1 = {},\n", B1::b1)
+                << std::format("\tB2::b2 = {},\n", B2::b2)
+                << std::format("\tC3::c3 = {};\n", c3);
+        }
+
+    protected:
+        int c3;
 };
 
 void endl (std::ostream& os = std::cout)
@@ -95,9 +197,15 @@ void endl (std::ostream& os = std::cout)
 int main (void)
 {
     A1 a1(1); a1.printn(); endl();
+    A2 a2(2); a2.printn(); endl();
+    A3 a3(3); a3.printn(); endl();
+
     B1 b1(2); b1.printn(); endl();
+    B2 b2(2); b2.printn(); endl();
+
     C1 c1(3); c1.printn(); endl();
     C2 c2(4); c2.printn(); endl();
+    C3 c3(4); c3.printn(); endl();
 
     A1 *base_ptr = &a1;
     base_ptr->printn();
